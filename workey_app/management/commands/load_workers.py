2ae0e68d3913bf1aebf766228f0d763cf68da7f8
daganida <daganida@post.bgu.ac.il>
2017-06-16 13:41:00 +0300

    Change a few comments and added atomic transaction on sub command load_workers.

--- workey_app/management/commands/load_workers.py
+++ workey_app/management/commands/load_workers.py
@@ -1,6 +1,7 @@
 import csv
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
 
 
 class Command(BaseCommand):
@@ -9,6 +10,7 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('file_path', type=str)
 
+    @transaction.atomic
     def handle(self, *args, **options):
         """
         Should parse the csv file into user records and insert them to database
@@ -24,8 +26,8 @@ class Command(BaseCommand):
                         try:
                             User.objects.create(first_name=user_record['FirstName'], last_name=user_record['LastName'],
                                                 username=user_record['Username'], password=user_record['Password'])
-                        except Exception as e:
+                        except Exception:
                             print 'Could not create  %s' % user_record['Username']
-        except Exception as e:
-            raise CommandError('Could not import from file %s' % file_path)
+        except Exception:
+            raise CommandError('Could not import worker records from file %s' % file_path)
         self.stdout.write(self.style.SUCCESS('Successfully imported tasks and workers from file %s ' % file_path))
