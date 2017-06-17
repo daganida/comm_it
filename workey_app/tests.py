7a021ad4af930afdb2728e1391d71d40e51372c6
daganida <daganida@post.bgu.ac.il>
2017-06-17 13:03:01 +0300

    Added test to support sub_command and test_data file in order to test it.

--- workey_app/tests.py
+++ workey_app/tests.py
@@ -11,17 +11,16 @@ class CommandsTestCase(TestCase):
 
     def test_load_workers_command(self):
         "Should test load_workers command perform insertion of all records into the database."
-        parent_parser = argparse.ArgumentParser(add_help=False)
-        parent_parser.add_argument('-file_path','--/Users/idandagan/Desktop/data_csv.csv' )
-        parser = argparse.ArgumentParser(add_help=False)
-        opts = parser.parse_args()
         workers_count = Worker.objects.all().count()
         self.assertEqual(workers_count, 0)
+        file_path = 'test_data.csv'
+        args = [file_path]
         out = StringIO()
-        call_command('load_workers', stdout=out, **opts)
-        self.assertIn('Expected output', out.getvalue())
+        opts = {}
+        call_command('load_workers', stdout=out, *args, **opts)
+        self.assertIn('Successfully', out.getvalue())
         import csv
-        with open(opts['file_path']) as csvfile:
+        with open(file_path) as csvfile:
             reader = csv.DictReader(csvfile)
             user_records = [record for record in reader]
             records_number = len(user_records)
